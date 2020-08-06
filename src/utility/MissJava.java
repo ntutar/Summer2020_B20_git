f2c2b3382a06775015d76364b47b267c8db46051
ntutar <68946955+ntutar@users.noreply.github.com>
2020-08-06 11:37:15 +1000

    Update MissJava.java

--- src/utility/MissJava.java
+++ src/utility/MissJava.java
@@ -3,6 +3,10 @@ package utility;
 public class MissJava {
     public static void main(String[] args) {
         System.out.println("I miss java");
+        
+        // I love java so much
+        // I added this line from Java
+            
 
 
 
