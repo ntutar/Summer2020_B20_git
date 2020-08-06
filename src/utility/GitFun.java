f10a8df3c7c46ec7ed066f756e22770a2689317b
ntutar <68946955+ntutar@users.noreply.github.com>
2020-08-06 11:30:49 +1000

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,6 +3,8 @@ package utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Everything is fun");
+        
+        // this line from github
     }
 
 }
